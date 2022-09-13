c2239c76d621ae57062b6f1c26b48148b7a62063
slykare <sylaninteriors@gmail.com>
2022-09-13 04:38:03 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -12,7 +12,7 @@ int main(void)
 {
 	int i = 0;
 	char c;
-	char s[] = "_putchar\n";
+	char s[] = "_putchar";
 
 	while (i <= 9)
 	{
@@ -20,5 +20,6 @@ int main(void)
 		_putchar(c);
 		i++;
 	}
+	putchar('\n')
 	return (0);
 }

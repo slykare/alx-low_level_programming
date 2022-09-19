d5360165c9bd73cbf6bae5cb098add38ab8f65d2
slykare <sylaninteriors@gmail.com>
2022-09-19 12:16:05 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swapping variables
+ * @a: pointer1
+ * @b: pointer2
+ */
+void swap_int(int *a, int *b);
+{
+
+	int d;
+
+	d = *a;
+	*a = *b;
+	*b = d;
+
+}

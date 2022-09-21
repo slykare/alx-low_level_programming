b8bb61c5c8d2a5fd667fda379074ed621de459c6
slykare <sylaninteriors@gmail.com>
2022-09-21 03:18:03 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings.
+ * @dest: an input string.
+ * @src: an input string.
+ * Return: a pointer to the resulting string dest.
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *temp = dest;
+
+	while (*dest)
+		dest++;
+
+	while (*src)
+		*dest++ = *src++;
+
+	*dest = '\0';
+	return (temp);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+readme for ore pointers and arrays 

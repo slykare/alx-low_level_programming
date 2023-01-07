ea28796cc9b94aa17d804166a16b74f9c5e42aef
slykare <sylaninteriors@gmail.com>
2023-01-07 04:08:08 -0800

    hash function implementing the djb2 algorithm

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,21 +1,25 @@
 #include "hash_tables.h"
 
 /**
- * hash_djb2 - function that generates a random number through
- * a string given (hash number)
- * @str: string
- * Return: hash number
- */
-
+  * hash_djb2 - hash function implementing the djb2 algorithm
+  * @str: The input string
+  * Return: hash
+  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash;
-    int c;
+	unsigned long int hash;
+	int c;
 
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-    return (hash);
+	hash = 5381;
+	/**
+	  * str++ is going to the next address in memory,where the next
+	  * char in the string is stored
+	  */
+	while ((c = *str++))
+	{
+		/* hash << 5 = hash * 2^5 */
+		hash = ((hash << 5) + hash) + c;    /* hash * 33 + c */
+	}
+
+	return (hash);
 }
